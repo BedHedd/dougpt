469d2fc123ef9750abc52a7e13b80fdae2e78f0d
bedhedd <zbedhedd@gmail.com>
2026-02-28 17:01:09 -0600

    chore: remove asr-runner from master (now a worktree)

--- 02-worktrees/asr-runner/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Schema module for ASR pipeline."""
-
-from .transcript import WordTiming, RawSegment, NormalizedSegment, TranscriptManifest
-
-__all__ = ["WordTiming", "RawSegment", "NormalizedSegment", "TranscriptManifest"]
